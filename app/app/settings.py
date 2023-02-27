042b1f08c3d42b4ed63a03b97e43afb17caeaf4e
brahim024 <ifninos168@gmail.com>
2023-02-27 11:19:58 +0000

    add settings

--- app/app/settings.py
+++ app/app/settings.py
@@ -25,13 +25,13 @@ SECRET_KEY = os.environ.get("SECRET_KEY",0)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
-ALLOWED_HOSTS = []
-ALLOWED_HOSTS.extend(
-    filter(
-        None,
-        os.environ.get("ALLOWED_HOSTS",'127.0.0.1').split(","),
-    )
-)
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+# ALLOWED_HOSTS.extend(
+#     filter(
+#         None,
+#         os.environ.get("ALLOWED_HOSTS",'127.0.0.1').split(","),
+#     )
+# )
 
 # Application definition
 
